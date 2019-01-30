79b2c8e883df8e44a5951bfe443369d2409321fb
williamsk91 <williamskusnandi@gmail.com>
2019-01-30 14:31:23 +0800

    removal of map component
    A map was to be implemented but the plan has been scraped

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React, { Component } from 'react';
 import AsyncSearchBar from './components/AsyncSearchBar'
 import Timeline from './components/Timeline'
 import { connect } from 'react-redux'
-import Map from './components/Map'
 
 class App extends Component {
 
   render() {
     const timeline = this.props.lat ? <Timeline lat={this.props.lat} lon={this.props.lon} /> : null;
-    // const map = this.props.lat ? <Map /> : null;
 
     return (
       <div className="App">
         <AsyncSearchBar />
-        {/* {map} */}
         {timeline}
       </div>
     );
